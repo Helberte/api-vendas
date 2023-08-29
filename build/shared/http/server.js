f368a1a25c87f376d08716dc049a350ee1d4d426
helberte <helberteads@gmail.com>
2023-08-28 22:42:26 -0300

    feat: criando o primeiro diretorio e primeira entity para a tabela products no typeorm

--- build/shared/http/server.js
+++ build/shared/http/server.js
@@ -3,13 +3,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./routes"));
+const AppError_1 = __importDefault(require("../errors/AppError"));
+require("../typeorm");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(routes_1.default);
+app.use((error, request, response, next) => {
+    if (error instanceof AppError_1.default) {
+        return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message
+        });
+    }
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+    });
+});
 app.listen(3333, () => {
-    console.log('Server started on port 3333!');
+    console.log('Server started on port 3333');
 });
---
+++ build/shared/typeorm/migrations/1693172682591-CreateProducts.js
@@ -0,0 +1,61 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreateProducts1693172682591 = void 0;
+const typeorm_1 = require("typeorm");
+class CreateProducts1693172682591 {
+    up(queryRunner) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield queryRunner.createTable(new typeorm_1.Table({
+                name: 'products',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()'
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'price',
+                        type: 'decimal',
+                        precision: 10,
+                        scale: 2
+                    },
+                    {
+                        name: 'quantity',
+                        type: 'int'
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp with time zone',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp with time zone',
+                        default: 'now()'
+                    }
+                ]
+            }));
+        });
+    }
+    down(queryRunner) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield queryRunner.dropTable('products');
+        });
+    }
+}
+exports.CreateProducts1693172682591 = CreateProducts1693172682591;
