9a00751d87e0e78dcc19b908ad6be236dddb0ab9
helberte <helberteads@gmail.com>
2023-08-31 22:37:40 -0300

    feat: adicionado validacao de parametros na requisicao a api no params e body com celebrate

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,13 +1,68 @@
 import { Router } from "express";
 import ProductsController from "../controlers/ProductsController";
+import { celebrate, Joi, Segments } from "celebrate"; 'celebrate';
 
 const productsRouter = Router();
 const productsControler = new ProductsController();
 
+// index
 productsRouter.get('/', productsControler.index);
-productsRouter.get('/:id', productsControler.show);
-productsRouter.post('/', productsControler.create);
-productsRouter.put('/:id', productsControler.update);
-productsRouter.delete('/:id', productsControler.delete);
+
+// show
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]:
+    {
+      id: Joi.string().uuid().required()
+    }
+  }),
+  productsControler.show
+);
+
+// create
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys(
+      {
+        name: Joi.string().required(),
+        price: Joi.number().precision(2).required(),
+        quantity: Joi.number().required()
+      }
+    )
+  }),
+  productsControler.create
+);
+
+// update
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]:
+    {
+      id: Joi.string().uuid().required()
+    },
+    [Segments.BODY]:
+    {
+      name:     Joi.string().required(),
+      price:    Joi.number().precision(2).required(),
+      quantity: Joi.number().required()
+    }
+  }),
+  productsControler.update
+);
+
+// delete
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]:
+    {
+      id: Joi.string().uuid().required()
+    }
+    }),
+  productsControler.delete
+);
 
 export default productsRouter;
