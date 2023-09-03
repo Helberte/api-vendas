5b178b53c2214daad635da41d8cdccb37f98fe89
helberte <helberteads@gmail.com>
2023-09-03 15:41:20 -0300

    feat: adicionado repositorio usersTokensRepository

---
+++ src/modules/users/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,26 @@
+import { EntityRepository, Repository } from "typeorm";
+import UserToken from "../entities/UserToken";
+
+@EntityRepository(UserToken)
+export class UsersTokensRepository extends Repository<UserToken>{
+
+  public async findByToken(token: string): Promise<UserToken | undefined>{
+    const userToken = await this.findOne({
+      where: {
+        token: token
+      }
+    });
+
+    return userToken;
+  }
+
+  public async generate(user_id: string): Promise<UserToken | undefined>{
+    const userToken = this.create({
+      user_id: user_id
+    });
+
+    await this.save(userToken);
+
+    return userToken;
+  }
+}
