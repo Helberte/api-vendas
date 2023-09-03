59d3f322f23abf4256f5f14e8f7fdccf3fbb9e71
helberte <helberteads@gmail.com>
2023-09-03 15:53:34 -0300

    feat: adicionado service para verificacao de email do usuario e geracao de token

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,27 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "src/shared/errors/AppError";
+import { UsersRepository } from "../typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "../typeorm/repositories/UsersTokensRepository";
+
+interface IRequest{
+  email: string;
+}
+
+class SendForgotPasswordEmailService{
+
+  public async execute({ email }: IRequest) : Promise<void> {
+    const userRepositoy   = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UsersTokensRepository);
+
+    const user = await userRepositoy.findByEmail(email);
+
+    if (!user)
+      throw new AppError('Email do usuário não encontrado na base.');
+
+    const userToken = await userTokensRepository.generate(user.id);
+
+    console.log(userToken);
+  }
+}
+
+export default SendForgotPasswordEmailService;
