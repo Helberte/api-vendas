c348813ebf315583d62f1f76bb5ccdc2beeb78b3
helberte <helberteads@gmail.com>
2023-09-05 13:28:10 -0300

    feat: adicionado servico de exibicao de usuario

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,25 @@
+import { getCustomRepository } from "typeorm";
+import User from "../typeorm/entities/User";
+import { UsersRepository } from "../typeorm/repositories/UsersRepository";
+import AppError from "src/shared/errors/AppError";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ShowProfileService{
+
+  public async execute({ user_id }: IRequest) : Promise<User | undefined> {
+    const userRepositoy = getCustomRepository(UsersRepository);
+
+    const user = await userRepositoy.findById(user_id);
+
+    if (user) {
+      throw new AppError('Usuário não encontrado.');
+    }
+
+    return user;
+  }
+}
+
+export default ShowProfileService;
