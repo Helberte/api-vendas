43d31d611d0f81e1d4baef91d7148bc726e9ec38
helberte <helberteads@gmail.com>
2023-09-11 22:55:21 -0300

    feat: inicio da criacao do servico de Order

---
+++ src/modules/orders/services/CreateOrderService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "src/shared/errors/AppError";
+import Order from "../typeorm/entities/Order";
+import { OrdersRepository } from "../typeorm/repositories/OrdersRepository";
+import { CustomersRepository } from "src/modules/customers/typeorm/repositories/CustomersRepository";
+import { ProductRepository } from "src/modules/products/typeorm/repositories/ProductsRepository";
+
+interface IProduct {
+  id: string;
+  quantity: number;
+}
+
+interface IRequest{
+  customer_id: string;
+  products: IProduct[];
+}
+
+class CreateOrderService{
+
+  public async execute({ customer_id, products }: IRequest) : Promise<Order> {
+    const ordersRepositoy   = getCustomRepository(OrdersRepository);
+    const customerRepositoy = getCustomRepository(CustomersRepository);
+    const productRepositoy  = getCustomRepository(ProductRepository);
+
+    const customerExists = await customerRepositoy.findById(customer_id);
+
+    if(!customerExists)
+      throw new AppError('Não existe este cliente.');
+
+    const product = productsRepositoy.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productsRepositoy.save(product);
+
+    return product;
+  }
+}
+
+export default CreateOrderService;
