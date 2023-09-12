45915dd937b92c40facea0a537aec2c7fd02d1bf
helberte <helberteads@gmail.com>
2023-09-11 22:36:32 -0300

    feat: adicionado relacionamentos de order, ordersProducts e Product

--- src/modules/orders/typeorm/entities/Order.ts
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -1,5 +1,6 @@
 import Customer from "src/modules/customers/typeorm/entities/Customers";
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import OrdersProducts from "./OrdersProducts";
 
 @Entity('orders')
 export default class Order{
@@ -10,6 +11,11 @@ export default class Order{
   @ManyToOne(() => Customer)
   @JoinColumn({name: 'customer_id'})
   customer: Customer;
+
+  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
+    cascade: true // toda vez que salvar, todos os orders products relacionados a esta ordem já serão salvos altomaticamente no banco de dados
+  })
+  order_products: OrdersProducts[];
 
   @CreateDateColumn()
   created_at: Date;
